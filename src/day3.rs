6f686cb78a1ee0f1e280b96ad9f4e3b82a27305c
Jonathon Fry <jonfry22@gmail.com>
2017-12-03 22:10:11 +0000

    WIp

--- src/day3.rs
+++ src/day3.rs
@@ -1,42 +1,67 @@
 use util::read;
 use std::collections::HashMap;
 
-struct Matrix(i32, i32, i32, f32);
+#[derive(PartialEq, Eq, Hash)]
+struct Pair {
+    x: i64,
+    y: i64,
+}
+
 
 pub fn solution() {
     println!("Day 3 part 1 {}", part1());
     println!("Day 3 part 2 {}", part2());
 }
 fn part1() -> i64 {
-    let data = read("day3".to_owned());
-    let size = data.parse::<i64>().unwrap();
-    let temp = size.to_owned();
-    let mut count = 0;
+    let data = read("day3".to_owned()).parse::<i64>().unwrap();
 
-    let square = (temp as f64).sqrt().round();
-    println!("{}", square);
-    let squareSize = (square * square) as i64;
-    println!("{}", squareSize);
-    println!("{}", size);
-    let x = square as i64 - (squareSize - size) - 1;
-    let y = square as i64 - 1;
-    println!("x,y {},{}", x, y);
+ /*
+    move x 1
+    move y 1
+    move x 2
+    move y 2
+    move x 3 
+    move y 3
 
-    let middle = (square as i64 - 1) / 2;
-
-    println!("middle x,y {},{}", middle, middle);
-    /*
-    17  16  15  14  13
-    18   5   4   3  12
-    19   6   1   2  11
-    20   7   8   9  10
-    21  22  23  24  25
     */
 
-    let temp1 = x - middle;
-    let temp2 = y - middle;
+    /*
+    x positive 1
+    y negative 1
+    x negative 2
+    y positive 2
+    x positive 3
+    y negative 3
+    */
+ 
+    let mut map: HashMap<Pair, i64> = HashMap::new();
+    let mut x: i64 = 0;
+    let mut y: i64 = 0;
+    let mut amount = 1;
+   
+    let mut current = Pair{x: 0, y: 0};
 
-    println!("a b {},{}", temp1, temp2);
+    for i in 0..data {
+        println!("{}", i);
+        map.insert(Pair { x: x, y: y }, i + 1);
+        x += amount;
+        y += amount;
+
+        current = Pair{ x: current.x + amount, y: current.y + amount};
+    }
+    /*
+    0,0
+    1,0
+    1,-1
+    0,-1
+    -1,-1
+    -1,0
+
+    */
+
+    // for (key, value) in map {
+    //     println!("{},{} | {}", key.x, key.x, value);
+    // }
 
     let mut sum = 0;
     return sum;

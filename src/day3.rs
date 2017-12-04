dee7c57e431f49f722814765e2ad3ea68ecfcb3c
Jonathon Fry <jonfry22@gmail.com>
2017-12-04 11:45:38 +0000

    Day 3 Part 1

--- src/day3.rs
+++ src/day3.rs
@@ -3,67 +3,60 @@ use std::collections::HashMap;
 
 #[derive(PartialEq, Eq, Hash)]
 struct Pair {
-    x: i64,
-    y: i64,
+    x: i32,
+    y: i32,
 }
-
 
 pub fn solution() {
     println!("Day 3 part 1 {}", part1());
     println!("Day 3 part 2 {}", part2());
 }
-fn part1() -> i64 {
+fn part1() -> i32 {
     let data = read("day3".to_owned()).parse::<i64>().unwrap();
-
- /*
-    move x 1
-    move y 1
-    move x 2
-    move y 2
-    move x 3 
-    move y 3
-
-    */
-
-    /*
-    x positive 1
-    y negative 1
-    x negative 2
-    y positive 2
-    x positive 3
-    y negative 3
-    */
  
-    let mut map: HashMap<Pair, i64> = HashMap::new();
-    let mut x: i64 = 0;
-    let mut y: i64 = 0;
+    let mut map: HashMap<i64, Pair> = HashMap::new();
+    let mut x: i32 = 0;
+    let mut y: i32 = 0;
     let mut amount = 1;
-   
-    let mut current = Pair{x: 0, y: 0};
+    let mut moveX = true;
+    let mut positive = true;
 
-    for i in 0..data {
-        println!("{}", i);
-        map.insert(Pair { x: x, y: y }, i + 1);
-        x += amount;
-        y += amount;
-
-        current = Pair{ x: current.x + amount, y: current.y + amount};
+    let mut i = 1;
+    map.insert(i, Pair { x: x, y: y });
+    'outer: loop {
+        if moveX {
+            for _j in 0..amount {
+                if i == data { break 'outer; }
+                if positive {
+                    x+= 1;  
+                } else{
+                    x-= 1;  
+                }
+                i+= 1;  
+                map.insert(i, Pair { x: x, y: y });
+            }
+            positive = !positive;
+        } else {
+            for _j in 0..amount {
+                if i == data { break 'outer; }
+                if positive{
+                    y+= 1;  
+                } else{
+                    y-= 1;  
+                }
+                
+                i+= 1;  
+                map.insert(i, Pair { x: x, y: y });
+            }
+            amount += 1;
+        }
+        moveX = !moveX;
+        if i == data { break 'outer; }
     }
-    /*
-    0,0
-    1,0
-    1,-1
-    0,-1
-    -1,-1
-    -1,0
-
-    */
-
-    // for (key, value) in map {
-    //     println!("{},{} | {}", key.x, key.x, value);
-    // }
-
-    let mut sum = 0;
+    
+    let vector = map.get(&data).unwrap();
+    
+    let mut sum = vector.x.abs() + vector.y.abs();
     return sum;
 }
 

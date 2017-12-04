9d00c9a77e6b172163ae814426507dba645bf277
Jonathon Fry <jonfry22@gmail.com>
2017-12-04 15:52:45 +0000

    Day 3 Part 1

--- src/day3.rs
+++ src/day3.rs
@@ -11,59 +11,19 @@ pub fn solution() {
     println!("Day 3 part 1 {}", part1());
     println!("Day 3 part 2 {}", part2());
 }
+
 fn part1() -> i32 {
-    let data = read("day3".to_owned()).parse::<i64>().unwrap();
+    let data = read("day3".to_owned());
+    let size = data.parse::<i64>().unwrap();
 
-    let mut map: HashMap<i64, Pair> = HashMap::new();
-    let mut x: i32 = 0;
-    let mut y: i32 = 0;
-    let mut amount = 1;
-    let mut moveX = true;
-    let mut positive = true;
+    let square = (size.to_owned() as f64).sqrt().round();
+    let square_size = (square * square) as i64;
+    let middle = (square as i64 - 1) / 2;
+    let x = square as i64 - (square_size - size) - 1;
+    let y = square as i64 - 1;
 
-    let mut i = 1;
-    map.insert(i, Pair { x: x, y: y });
-    'outer: loop {
-        if moveX {
-            for _j in 0..amount {
-                if i == data {
-                    break 'outer;
-                }
-                if positive {
-                    x += 1;
-                } else {
-                    x -= 1;
-                }
-                i += 1;
-                map.insert(i, Pair { x: x, y: y });
-            }
-            positive = !positive;
-        } else {
-            for _j in 0..amount {
-                if i == data {
-                    break 'outer;
-                }
-                if positive {
-                    y += 1;
-                } else {
-                    y -= 1;
-                }
-
-                i += 1;
-                map.insert(i, Pair { x: x, y: y });
-            }
-            amount += 1;
-        }
-        moveX = !moveX;
-        if i == data {
-            break 'outer;
-        }
-    }
-
-    let vector = map.get(&data).unwrap();
-
-    let mut sum = vector.x.abs() + vector.y.abs();
-    return sum;
+    let sum = (x - middle).abs() + (y - middle).abs();
+    return sum as i32;
 }
 
 fn part2() -> i32 {

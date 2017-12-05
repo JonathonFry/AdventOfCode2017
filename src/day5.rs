8ef19963a87fc6b0b8a6ceb2abad528c4284e9c6
Jonathon Fry <jonfry22@gmail.com>
2017-12-05 09:06:49 +0000

    Day 5 part 1

--- src/day5.rs
+++ src/day5.rs
@@ -1,36 +1,39 @@
 use util::read;
 
 pub fn solution() {
-    let data = read("day1".to_owned());
+    let data = read("day5".to_owned());
     let vec = data.split("\n")
         .map(|x| x.parse::<i32>().unwrap())
         .collect::<Vec<i32>>();
 
-    println!("Day 5 part 1 {}", part1(&vec));
+    println!("Day 5 part 1 {}", part1(vec));
 //    println!("Day 5 part 2 {}", part2());
 }
 
-fn part1(vec: &Vec<i32>) -> u32 {
-    let mut input = vec;
+fn part1(vec: Vec<i32>) -> u32 {
+    let mut input = vec.clone();
+
     let mut steps = 0;
     let mut value = 0;
+
     loop {
         if value >= input.len() {
             break;
         }
 
-        let temp = &mut input[value];
-        temp = temp + 1;
+        let temp = input[value];
+        input[value] += 1;
 
+        let new_value = value as i32 + temp;
+        value = new_value as usize;
 
         steps += 1;
-        if steps == 1000 { break; }
     }
 
     return steps;
 }
 
 fn part2() -> u32 {
-    let mut sum = 0;
+    let sum = 0;
     return sum;
 }

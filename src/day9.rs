077b4d7066c015faef8090dad642d7129fb49db0
Jonathon Fry <jonfry22@gmail.com>
2017-12-12 12:34:54 +0000

    Day 9 wip

--- src/day9.rs
+++ src/day9.rs
@@ -15,17 +15,20 @@ fn solve(level: u32, input: &str) -> u32 {
     println!("level: {}, input: {}", level, input);
     let mut value = level + 1;
 
+    let has_groups = contains_groups(input);
+    println!("input: {} contains groups: {}", input, has_groups);
+
     let groups = get_groups(input);
     // if it doesn't contain any groups and is valid
 
-    if groups.len() > 1 {
+    if valid(input) {
+        return value;
+    } else if groups.len() > 1 {
         for group in groups {
             value += solve(level + 1, group.as_ref());
         }
     } else {
-        if !valid(input) {
-            return 0;
-        }
+        return 0;
     }
 
     println!("value: {}", value);
@@ -60,4 +63,27 @@ fn valid(input: &str) -> bool {
     let re = Regex::new("<+[^>]*>|\\{}").unwrap();
     let valid = re.is_match(input);
     return valid;
+}
+
+fn contains_groups(input: &str) -> bool {
+    // Remove first and last character if {}
+    // Copy everything from first bracket to
+    /*
+    {{}}
+    find first closing bracket, match with last opening bracket (substring)
+    */
+    let mut groups: Vec<String> = Vec::new();
+    let buf = String::new();
+    let mut index = 0;
+    for c in input.chars() {
+        if  c.to_string() == "}" {
+
+        }
+        if c.to_string() == "}" && index as usize != input.len() {
+            return true;
+        }
+        index += 1;
+    }
+
+    return false;
 }

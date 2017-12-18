949a6543e86b264f656d873325db6b3c720170c1
Jonathon Fry <jonfry22@gmail.com>
2017-12-18 11:31:59 +0000

    Day 11 Part 1

--- src/day11.rs
+++ src/day11.rs
@@ -4,23 +4,30 @@ pub fn solution() {
     let input: Vec<String> = read("day11".to_owned()).split(",").map(|s| s.to_string()).collect();
     let mut x = 0;
     let mut y = 0;
+    let mut z = 0;
 
     for i in input {
-        let (new_x, new_y) = calc(i.as_ref());
+        let (new_x, new_y, new_z) = calc(i.as_ref());
         x += new_x;
         y += new_y;
+        z += new_z;
     }
-    println!("x {}, y {}", x, y);
+
+    let value = (x.abs() + y.abs() + z.abs()) / 2;
+
+    println!("x {}, y {}, z {}", x, y, z);
+    println!("value {}", value);
 }
 
-fn calc(direction: &str) -> (i32, i32) {
+
+fn calc(direction: &str) -> (i32, i32, i32) {
     return match direction {
-        "nw" => (-1, 1),
-        "n" => (0, 1),
-        "ne" => (1, 1),
-        "se" => (1, -1),
-        "s" => (0, -1),
-        "sw" => (-1, -1),
+        "nw" => (-1, 1, 0),
+        "n" => (0, 1, -1),
+        "ne" => (1, 0, -1),
+        "se" => (1, -1, 0),
+        "s" => (0, -1, 1),
+        "sw" => (-1, 0, 1),
         _ => panic!("OMG")
     };
 }

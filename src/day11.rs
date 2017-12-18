d426d1485a3516a538f21b759dc0b47e721a9ed5
Jonathon Fry <jonfry22@gmail.com>
2017-12-18 11:33:21 +0000

    Day 11 Part 2

--- src/day11.rs
+++ src/day11.rs
@@ -6,17 +6,23 @@ pub fn solution() {
     let mut y = 0;
     let mut z = 0;
 
+    let mut max = 0;
+
     for i in input {
         let (new_x, new_y, new_z) = calc(i.as_ref());
         x += new_x;
         y += new_y;
         z += new_z;
+        let temp = (x.abs() + y.abs() + z.abs()) / 2;
+        if temp > max {
+            max = temp;
+        }
     }
 
     let value = (x.abs() + y.abs() + z.abs()) / 2;
 
-    println!("x {}, y {}, z {}", x, y, z);
-    println!("value {}", value);
+    println!("Day 11 Part 1 {}", value);
+    println!("Day 11 Part 2 {}", max);
 }
 
 

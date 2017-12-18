f7884eef414c89e844d4b88c24f2a0550bfcbcc2
Jonathon Fry <jonfry22@gmail.com>
2017-12-18 09:37:24 +0000

    Day 10 part 2

--- src/day10.rs
+++ src/day10.rs
@@ -1,55 +1,75 @@
 use std::ops::Range;
-use std::ascii::AsciiExt;
-
 
 pub fn solution() {
-    let temp = b"212,254,178,237,2,0,1,54,167,92,117,125,255,61,159,164";
+    part_one();
+    part_two();
+}
 
-    //    let input = [212, 254, 178, 237, 2, 0, 1, 54, 167, 92, 117, 125, 255, 61, 159, 164];
-    let mut input: Vec<u32> = temp.iter().map(|x| format!("{:?}", x).parse::<u32>().unwrap()).collect();
-    let extra: Vec<u32> = [17, 31, 73, 47, 23].iter().map(|x| x.to_owned() as u32).collect();
-    input.extend(extra);
+fn part_one() {
+    let mut data: Vec<u32> = (0..256).collect();
 
-    let mut data: Vec<u32> = Range { start: 0, end: 256 }.collect();
-
+    let input = [212, 254, 178, 237, 2, 0, 1, 54, 167, 92, 117, 125, 255, 61, 159, 164];
     let mut position = 0;
     let mut skip_size = 0;
     let length = data.len();
 
     for i in input.iter() {
-        for x in 0..64 {
-            let mut temp: Vec<u32> = Vec::new();
-            for j in 0..i.clone() {
-                let pos = (position + j) % length as u32;
-                temp.push(data[pos as usize]);
-            }
-            temp = temp.iter().rev().cloned().collect();
+        let data_clone = &data.clone().to_owned();
 
-            for j in 0..i.clone() {
-                let pos = (position + j) % length as u32;
-                data[pos as usize] = temp[j as usize].to_owned();
-            }
+        let mut temp: Vec<u32> = Vec::new();
+        for j in 0..i.clone() {
+            let pos = (position + j) % length as i32;
+            temp.push(data_clone[pos as usize]);
+        }
+        temp = temp.iter().rev().cloned().collect();
+
+        for j in 0..i.clone() {
+            let pos = (position + j) % length as i32;
+            data[pos as usize] = temp[j as usize].to_owned();
         }
 
         position += i + skip_size;
         skip_size += 1;
     }
+    let part_one = data[0] * data[1];
 
-    let mut answer: Vec<u32> = Vec::new();
-    let mut it = 0;
-    while it < length {
-        let arr: Vec<&u32> = data.iter().skip(it).take(16).collect();
-        let mut value = 1;
-        for x in arr.iter().skip(1) {
-            value ^= x.to_owned();
+    println!("Day 10 Part 1 {:?}", part_one);
+}
+
+fn part_two() {
+    let temp = b"212,254,178,237,2,0,1,54,167,92,117,125,255,61,159,164";
+    let mut input: Vec<u32> = temp.iter().map(|x| format!("{:?}", x).parse::<u32>().unwrap()).collect();
+    let extra: Vec<u32> = [17, 31, 73, 47, 23].iter().map(|x| x.to_owned() as u32).collect();
+    input.extend(extra);
+
+    let mut data: Vec<u32> = (0..256).collect();
+
+    let mut position = 0;
+    let mut skip_size = 0;
+    let len = data.len() as u32;
+
+    for _round in 0..64 {
+        for length in input.iter() {
+            for i in 0..(length / 2) {
+                data.swap(((position + i) % len) as usize, ((position + length - i - 1) % len) as usize);
+            }
+
+            position += length + skip_size;
+            skip_size += 1;
         }
-        answer.push(value);
-        it += 16;
     }
 
-    let axc: String = answer.iter().map(|x| format!("{:x}", x)).collect();
+    let hash: String = data.chunks(16)
+        .map(|chunk| chunk.iter().fold(0, |acc, &v| acc ^ v as u8))
+        .map(|v| {
+            let mut t = format!("{:x}", v);
+            if t.len() == 1 {
+                t.insert(0, '0');
+            }
+            t
+        })
+        .collect::<Vec<String>>()
+        .join("");
 
-    println!("{}", axc);
-
-    println!("data {:?}", data);
+    println!("Day 10 Part 2 {}", hash);
 }
